c29d505e484d421c1f683353d411caf902b146e1
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-27 19:59:09 +0600

    added jobs get api method

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+
+
+    const jobsCollection = client.db('jobPortal').collection('jobs');
+
+    // jobs api
+    app.get('/jobs', async(req, res) =>{
+        const cursor = (await jobsCollection).find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
